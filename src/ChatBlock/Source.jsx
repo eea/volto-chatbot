27e62660ab6d5f3a7e7bd09d185b33a7e79cf57d
kreafox <krisztina.n.elekes@gmail.com>
2024-09-16 12:36:36 +0300

    style: improve sources section - refs #277196

--- src/ChatBlock/Source.jsx
+++ src/ChatBlock/Source.jsx
@@ -1,35 +1,55 @@
+import { Popup } from 'semantic-ui-react';
 import { DateTime } from 'luxon';
 import { SVGIcon } from './utils';
 
 import FileIcon from './../icons/file.svg';
 import GlobeIcon from './../icons/globe.svg';
 
-export const SourceDetails = ({
-  source: {
-    semantic_identifier = 'untitled document',
-    updated_at,
+export const SourceDetails = ({ source, index }) => {
+  const {
+    link,
     blurb,
+    updated_at,
     source_type,
-  } = {},
-  index,
-}) => {
+    // match_highlights,
+    semantic_identifier = 'untitled document',
+  } = source || {};
   const parsedDate = updated_at ? DateTime.fromISO(updated_at) : null;
   const relativeTime = parsedDate?.toRelative();
+  const isLinkType = source_type === 'web';
+  const isDocumentType = source_type === 'file';
 
   const renderIcon = () => {
-    if (source_type === 'file')
-      return <SVGIcon name={FileIcon} size="18" alt="File icon" />;
-    if (source_type === 'web')
+    if (isLinkType) {
       return <SVGIcon name={GlobeIcon} size="18" alt="Web icon" />;
+    }
+    if (isDocumentType) {
+      return <SVGIcon name={FileIcon} size="18" alt="File icon" />;
+    }
     return null;
   };
 
   return (
     <div className="source">
       <div className="source-header">
-        {index && <span className="chat-citation">{index}</span>}{' '}
-        <div className="source-title">{semantic_identifier}</div>
-        {renderIcon()}
+        {isLinkType ? (
+          <a href={link} rel="noreferrer" target="_blank">
+            <span className="chat-citation">{index}</span>
+            <div className="source-title">{semantic_identifier}</div>
+            {renderIcon()}
+          </a>
+        ) : (
+          <>
+            <Popup
+              on="click"
+              wide="very"
+              content="This doc doesn't have a link."
+              trigger={<span className="chat-citation">{index}</span>}
+              popper={{ id: 'chat-citation-popup' }}
+            />
+            {renderIcon()}
+          </>
+        )}
       </div>
       {updated_at && (
         <div className="source-date">
